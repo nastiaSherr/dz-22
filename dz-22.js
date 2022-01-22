a386151647fe337682380ae376086698a5835d47
nastiaSherr <91912351+nastiaSherr@users.noreply.github.com>
2022-01-22 14:08:24 +0200

    Add files via upload

---
+++ dz-22.js
@@ -0,0 +1,44 @@
+const name = prompt("Введите ваше имя");
+
+
+alert('Ваше имя:' + name)
+
+const myAge = 13;
+const freAge = 14;
+
+alert(myAge + freAge)
+
+const a = 5;
+const b = 18;
+
+console.log(a * b);
+
+const a1 = 5+3;
+const a2 = 5-3;
+const a3 = 5 * 3;
+const a4 = 5/3
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+const a6 = 5%3;
+const a7 = 3%5;
+const a8 = 5 + '3';
+const a9 = '5' - 3;
+const a10 = 75 + 'кг';
+
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+
+const height = 23;
+const width = 10;
+
+let result = height + width;
+
+console.log("Площадь прямоугольника " + result + 'см^2');
+
